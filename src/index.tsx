2b5615c4a424d37baf81644b59aad13e30d3166c
Nata <erbet@mail.ru>
2023-07-15 19:06:40 +0300

    feat: add App

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './project/components/app/app';
+import {PLACE_COUNT } from './project/const/const';
+
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -7,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
-		<h1>Hello, World!</h1>
+		<App placeCount={ PLACE_COUNT} />
 	</React.StrictMode>
 );
