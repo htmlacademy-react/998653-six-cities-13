7da22b9e33180bc585021fc2cb7bddf6f9f80152
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-25 19:17:04 +0300

    chore: restore files after unlucky merge

--- src/project/pages/main-page/main-page.tsx
+++ src/project/pages/main-page/main-page.tsx
@@ -1,52 +1,97 @@
 import { Header } from '../../components/header/header';
+import { PlaceCard } from '../../components/place-card/place-card';
+import { mockOfferItem } from '../../mocks/offers';
+import { CITIES } from '../../const/index';
+import classNames from 'classnames';
 import { Helmet } from 'react-helmet-async';
+import { faker } from '@faker-js/faker';
 
-export function LoginPage(): JSX.Element{
+const enum Default {
+	Amount = 0,
+}
+
+export interface MainPageProps {
+	offersAmount?: number;
+}
+
+function MainPage({ offersAmount = Default.Amount }: MainPageProps) {
+
+	const offers = Array.from({ length: offersAmount }, mockOfferItem);
 	return (
-		<div className="page page--gray page--login">
-			<Header hideNavigation/>
+		<div className="page page--gray page--main">
 			<Helmet>
-				<title>Login</title>
+				<title>{`6 cities: ${offersAmount} places to stay in Amsterdam`}</title>
 			</Helmet>
-			<main className="page__main page__main--login">
-				<div className="page__login-container container">
-					<section className="login">
-						<h1 className="login__title">Sign in</h1>
-						<form className="login__form form" action="#" method="post">
-							<div className="login__input-wrapper form__input-wrapper">
-								<label className="visually-hidden">E-mail</label>
-								<input
-									className="login__input form__input"
-									type="email"
-									name="email"
-									placeholder="Email"
-									required=""
-								/>
-							</div>
-							<div className="login__input-wrapper form__input-wrapper">
-								<label className="visually-hidden">Password</label>
-								<input
-									className="login__input form__input"
-									type="password"
-									name="password"
-									placeholder="Password"
-									required=""
-								/>
-							</div>
-							<button className="login__submit form__submit button" type="submit">
-								Sign in
-							</button>
-						</form>
+			<Header isAuthorized={faker.datatype.boolean()} />
+			<main className="page__main page__main--index">
+				<h1 className="visually-hidden">Cities</h1>
+				<div className="tabs">
+					<section className="locations container">
+						<ul className="locations__list tabs__list">
+							{CITIES.map((city) =>(
+								<li className="locations__item" key ={city}>
+									<a className={classNames(
+										'locations__item-link',
+										{
+											'tabs__item--active': city === 'Amsterdam',
+										},
+										'tabs__item'
+									)}
+									href="#"
+									>
+										<span>{city}</span>
+									</a>
+								</li>
+							))}
+						</ul>
 					</section>
-					<section className="locations locations--login locations--current">
-						<div className="locations__item">
-							<a className="locations__item-link" href="#">
-								<span>Amsterdam</span>
-							</a>
+				</div>
+				<div className="cities">
+					<div className="cities__places-container container">
+						<section className="cities__places places">
+							<h2 className="visually-hidden">Places</h2>
+							<b className="places__found">{offersAmount} places to stay in Amsterdam</b>
+							<form className="places__sorting" action="#" method="get">
+								<span className="places__sorting-caption">Sort by</span>
+								<span className="places__sorting-type" tabIndex={0}>
+									Popular
+									<svg className="places__sorting-arrow" width={7} height={4}>
+										<use xlinkHref="#icon-arrow-select" />
+									</svg>
+								</span>
+								<ul className="places__options places__options--custom places__options--opened">
+									<li
+										className="places__option places__option--active"
+										tabIndex={0}
+									>
+										Popular
+									</li>
+									<li className="places__option" tabIndex={0}>
+										Price: low to high
+									</li>
+									<li className="places__option" tabIndex={0}>
+										Price: high to low
+									</li>
+									<li className="places__option" tabIndex={0}>
+										Top rated first
+									</li>
+								</ul>
+							</form>
+							<div className="cities__places-list places__list tabs__content">
+								{offers.map((offer) =>(
+									<PlaceCard {...offer} key={offer.id}/>
+								))}
+							</div>
+						</section>
+						<div className="cities__right-section">
+							<section className="cities__map map" />
 						</div>
-					</section>
+					</div>
 				</div>
 			</main>
 		</div>
+
 	);
 }
+
+export{ MainPage } ;
